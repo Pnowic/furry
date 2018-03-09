0df7616145d88d8c708fad048c88a291af754245
Piotr Nowicki <nowickipiotrus@gmail.com>
2018-03-09 18:32:31 +0100

    add moveFurry function and hideVisibleFurry function

--- js/app.js
+++ js/app.js
@@ -14,4 +14,61 @@ var Game = function() {
     this.furry = new Furry();
     this.coin = new Coin();
     this.score = 0;
+    this.furryIndex = function (x, y) {
+        return x + (y * 10);
+    };
+    this.coinIndex = function (x, y) {
+        return x + (y * 10);
+
+    };
+    this.showFurry = function () {
+        this.board[ this.furryIndex(this.furry.x,this.furry.y) ].classList.add('furry');
+    };
+
+    this.showCoin = function () {
+        this.board[ this.coinIndex(this.coin.x,this.coin.y) ].classList.add('coin');
+    };
+
+
+    var self = this;
+
+    this.startGame = function() {
+        this.interval250 = setInterval(function() {
+            self.moveFurry();
+        }, 250);
+    };
+
+    this.moveFurry = function() {
+        this.hideVisibleFurry();
+        if (this.furry.direction === 'right') {
+            this.furry.x = this.furry.x + 1;
+        }
+        else if (this.furry.direction === 'left') {
+            this.furry.x = this.furry.x - 1;
+        }
+        else if (this.furry.direction === 'up') {
+            this.furry.y = this.furry.y - 1;
+        }
+        else if (this.furry.direction === 'down') {
+            this.furry.y = this.furry.y + 1;
+        }
+        this.showFurry();
+    };
+
+    this.hideVisibleFurry = function (){
+        var furryDiv = document.querySelector('div.furry');
+        furryDiv.classList.remove('furry');
+    }
+
+
+
+
+
+
+
 };
+
+var game = new Game();
+game.showFurry();
+game.showCoin();
+game.startGame();
